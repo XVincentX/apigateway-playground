be76226a8b014c4cf498e057c5292943405a2836
XVincentX <vincenzo@apiary.io>
2017-07-02 15:54:34 +0200

    Do not expose ports

--- s1.js
+++ s1.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.get('/status', (req, res) => {
+  res.sendStatus(204);
+});
 
 app.get('/', (req, res) => {
-  console.log("s2 hit");
   res.json({
     name: "Vincenzo",
     surname: "Chianese"
   });
 });
+
 
 app.listen(3000, () => { console.log("Ready to go!"); });
--- s2.js
+++ s2.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.get('/status', (req, res) => {
+  res.sendStatus(204);
+});
 
 app.get('/', (req, res) => {
-  console.log("s2 hit");
   res.json({
     invoiceId: "10",
     amount: 3000
   });
 });
+
 
 app.listen(3000, () => { console.log("Ready to go!"); });
