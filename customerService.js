7eb39d5a22b27d96f5d4ef145c9062ef86264c72
XVincentX <vincenzo@apiary.io>
2017-07-04 15:24:32 +0200

    Add body-parser dependency

--- customerService.js
+++ customerService.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 mongoose.Promise = global.Promise;
 
 const Customer = mongoose.model('customer', {
@@ -15,6 +16,8 @@ const Invoice = mongoose.model('invoice', {
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/customers/:id', (req, res) => {
   let query = {};
 
@@ -22,9 +25,18 @@ app.get('/customers/:id', (req, res) => {
     query._id = req.query.id;
 
   Customer.find(query).lean()
-    .then((customers) =>
+    .then((customers) => {
       res.json(customers);
-    ).catch((err) => {
+    }).catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.post('/customer/', (req, res) => {
+  const cust = new Customer(req.body);
+  cust.save((entity) => {
+    res.status(201).send({ id: entity._id });
+  }).catch((err) => {
     res.status(500).send(err);
   });
 });
