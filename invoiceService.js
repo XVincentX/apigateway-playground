d27f661b869ebf07b7743f8af3c7df6f4f31ea3d
XVincentX <vincenzo@apiary.io>
2017-07-20 14:29:04 +0200

    Cleanup invoice service

--- invoiceService.js
+++ invoiceService.js
@@ -2,12 +2,4 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({
-    invoiceId: "10",
-    amount: 3000
-  });
-});
-
-
 app.listen(3000, () => { console.log("Ready to go!"); });
