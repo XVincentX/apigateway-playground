793e43673385932afc3df6190edb262518e9ff70
Vincenzo Chianese <vincenz.chianese@icloud.com>
2017-11-24 18:53:21 +0100

    A new start

--- customerService.js
+++ customerService.js
@@ -10,12 +10,6 @@ const Customer = mongoose.model('customers', {
   surname: String
 });
 
-const Invoice = mongoose.model('invoices', {
-  date: Date,
-  amount: Number,
-  customer: [{ type: mongoose.Schema.Types.ObjectId, ref: 'customer' }]
-});
-
 const app = express();
 
 const checkRole = (roleType) => {
@@ -40,11 +34,9 @@ app.use(apikey(function (key, next) {
   return next(null, { role: 0 });
 }));
 
-
-
 app.use(bodyParser.json());
 
-app.get('/customers/:id?', checkRole('user'), (req, res) => {
+app.get('/:id?', checkRole('user'), (req, res) => {
   let query = {};
 
   if (req.params.id)
@@ -56,49 +48,12 @@ app.get('/customers/:id?', checkRole('user'), (req, res) => {
     (err) => res.status(500).send(err));
 });
 
-app.post('/customers/', checkRole('admin'), (req, res) => {
+app.post('/', checkRole('admin'), (req, res) => {
   (new Customer(req.body))
     .save()
     .then(
     (entity) => res.status(201).send({ id: entity._id }),
     (err) => res.status(500).send(err));
-});
-
-app.get('/customers/:customerId/invoices/:invoiceId?', checkRole('user'), (req, res) => {
-  if (!req.params.customerId)
-    return res.status(400).send("CustomerID hasn't been provided");
-
-  let query = { customer: req.params.customerId };
-
-  if (req.params.invoiceId)
-    query._id = req.params.invoiceId;
-
-  Invoice.find(query).lean()
-    .then(
-    (invoices) => res.json(invoices),
-    (err) => res.status(500).send(err));
-});
-
-app.post('/customers/:customerId/invoices/', checkRole('admin'), (req, res) => {
-
-  if (!req.params.customerId)
-    return res.status(400).send("CustomerID hasn't been provided");
-
-  Customer.count({ _id: req.params.customerId }).then(
-    (count) => {
-      if (count === 0)
-        return res.status(404).send("No customer found");
-
-      req.body.customer = req.params.customerId;
-
-      (new Invoice(req.body))
-        .save()
-        .then((entity) => res.status(201).send({ id: entity._id }),
-        (err) => res.status(500).send(err));
-
-    },
-    (err) => res.status(500).send(err));
-
 });
 
 mongoose.connect("mongodb://mongo/application", { useMongoClient: true })
