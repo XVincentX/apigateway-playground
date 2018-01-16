79031599260600bdcebdb9b69a2df90389bbf9fc
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-01-16 17:10:14 +0100

    Save is not required anymore

--- customerService.js
+++ customerService.js
@@ -58,7 +58,6 @@ app.get('/customers/:id?', checkRole('user'), (req, res) => {
 
 app.post('/customers/', checkRole('admin'), (req, res) => {
   Customer.create(req.body)
-    .save()
     .then(
     (entity) => res.status(201).send({ id: entity._id }),
     (err) => res.status(500).send(err));
@@ -92,7 +91,6 @@ app.post('/customers/:customerId/invoices/', checkRole('admin'), (req, res) => {
       req.body.customer = req.params.customerId;
 
       Invoice.create(req.body)
-        .save()
         .then((entity) => res.status(201).send({ id: entity._id }),
         (err) => res.status(500).send(err));
 
